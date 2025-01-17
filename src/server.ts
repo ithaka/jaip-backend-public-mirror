8483cef81f6be091bd2290c66c887cfc31fc0239
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-01-17 10:45:44 -0800

    Remove trailing slash

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ const fastify: FastifyInstance = Fastify({
 });
 
 fastify.get("/secret", opts, async () => {
-  const url = "http://localhost:8888/v1/apps/PDF-DELIVERY-SERVICE/";
+  const url = "http://localhost:8888/v1/apps/PDF-DELIVERY-SERVICE";
   let str = "";
   try {
     const response = await fetch(url);
