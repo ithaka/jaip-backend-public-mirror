e2ef98224331cd15cced40ba41c54b0d4fc6009b
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-01-21 15:00:34 -0800

    Modify some auth stuff

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -41,18 +41,29 @@ const getSessionManagement = async (
 const manageSession = async (
   fastify: FastifyInstance,
   request: FastifyRequest,
-): Promise<AxiosResponse<any, any>> => {
-  const sg = await getSessionManagement(fastify);
-  const uuid = request.cookies.uuid || "";
-  console.log("UUID: ", uuid);
+): Promise<string> => {
+  let uuid = request.cookies.uuid || "";
+  try {
+    const sg = await getSessionManagement(fastify);
+    if (!sg) throw new Error("Service discovery failed: No route found");
+    const query = uuid
+      ? `mutation { session(uuid: "${uuid}") { uuid }}`
+      : `mutation { session { uuid }}`;
+    const response = await axios.post(sg + "v1/graphql", {
+      query,
+    });
+    if (response.status !== 200) {
+      throw new Error("Session management failed: Status code not 200");
+    }
+    if (!response.data) {
+      throw new Error("Session management failed: No uuid returned");
+    }
+    uuid = response.data;
+  } catch (err) {
+    console.log(err);
+  }
 
-  const query = uuid
-    ? `mutation { session(uuid: "${uuid}") { uuid }}`
-    : `mutation { session { uuid }}`;
-  const response = await axios.post(sg + "v1/graphql", {
-    query,
-  });
-  return response;
+  return uuid;
 };
 
 async function routes(fastify: FastifyInstance, opts: RouteShorthandOptions) {
@@ -64,13 +75,12 @@ async function routes(fastify: FastifyInstance, opts: RouteShorthandOptions) {
       let uuid = "";
       try {
         const session = await manageSession(fastify, request);
-        uuid = session.data;
+        uuid = session;
       } catch (err) {
         console.log(err);
       }
 
       return {
-        hello: "world",
         uuid: uuid,
       };
     },
