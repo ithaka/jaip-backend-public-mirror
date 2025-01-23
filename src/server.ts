f0671f6d1c58fed94164b8ec62a0e5903c5c9d95
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-01-23 13:02:27 -0800

    Add a test db connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
 import fc from "@fastify/cookie";
+import fp from "@fastify/postgres";
 import "dotenv/config";
 import routes from "./routes";
 import decorators from "./decorators";
@@ -20,9 +21,22 @@ for (const route of routes) {
   fastify.register(route, opts);
 }
 
+const db_url = `postgres://${process.env.JAIP_DB_USERNAME}:${process.env.JAIP_DB_PASSWORD}@${process.env.JAIP_DB_LOCATION}/${process.env.JAIP_DB_NAME}`;
+fastify.register(fp, {
+  connectionString: db_url,
+});
+
+fastify.get("/names", (req, reply) => {
+  fastify.pg.query(
+    "SELECT id, name FROM entities",
+    function onResult(err, result) {
+      reply.send(err || result);
+    },
+  );
+});
+
 const start = async () => {
   try {
-    console.log(process.env);
     await fastify.listen({ port: 8080, host: "0.0.0.0" });
     const address = fastify.server.address();
     fastify.log.info(address);
