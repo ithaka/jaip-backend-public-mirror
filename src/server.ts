5dcfb04433d72efdc60c53bd7d2a7f215583e93a
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-01-24 07:23:56 -0800

    Use port for db connection

--- src/server.ts
+++ src/server.ts
@@ -21,29 +21,12 @@ for (const route of routes) {
   fastify.register(route, opts);
 }
 
-const db_url = `postgres://${process.env.JAIP_DB_USERNAME}:${process.env.JAIP_DB_PASSWORD}@${process.env.JAIP_DB_LOCATION}:3306/${process.env.JAIP_DB_NAME}`;
+const db_url = `postgres://${process.env.JAIP_DB_USERNAME}:${process.env.JAIP_DB_PASSWORD}@${process.env.JAIP_DB_LOCATION}:${process.env.JAIP_DB_PORT}/${process.env.JAIP_DB_NAME}`;
 fastify.register(fp, {
   connectionString: db_url,
+  name: "jaip_db",
 });
 
-fastify.get("/names", async (req, reply) => {
-  console.log("Starting DB Connection");
-  console.log(db_url);
-  const client = await fastify.pg.connect();
-  console.log("Client: ");
-  console.log(client);
-  try {
-    const { rows } = await client.query("SELECT id, name FROM entities");
-    console.log(rows);
-    return rows;
-  } catch (err) {
-    console.log("DB Connection Error: ");
-    console.log(err);
-  } finally {
-    // Release the client immediately after query resolves, or upon error
-    client.release();
-  }
-});
 const start = async () => {
   try {
     await fastify.listen({ port: 8080, host: "0.0.0.0" });
