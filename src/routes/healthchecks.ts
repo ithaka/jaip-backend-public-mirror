6571c437701febca842a533517a5165c4a1869ea
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-01-24 10:06:26 -0800

    Move healthcheck schema

--- src/routes/healthchecks.ts
+++ src/routes/healthchecks.ts
@@ -2,20 +2,6 @@ import { FastifyInstance, RouteShorthandOptions } from "fastify";
 import axios from "axios";
 import { SWAGGER_TAGS } from "../utils/swagger_tags";
 
-const schema = {
-  description: `Returns health information for the service, including indicators for service discovery and database access.`,
-  tags: [SWAGGER_TAGS.healthcheck],
-  response: {
-    200: {
-      type: "object",
-      properties: {
-        up: { type: "boolean" },
-        service_discovery: { type: "boolean" },
-      },
-    },
-  },
-};
-
 const polarisHealthcheck = async () => {
   const url = "http://localhost:8888/healthcheck";
   let up = false;
@@ -29,7 +15,19 @@ const polarisHealthcheck = async () => {
 };
 
 async function routes(fastify: FastifyInstance, opts: RouteShorthandOptions) {
-  opts.schema = schema;
+  opts.schema = {
+    description: `Returns health information for the service, including indicators for service discovery and database access.`,
+    tags: [SWAGGER_TAGS.healthcheck],
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          up: { type: "boolean" },
+          service_discovery: { type: "boolean" },
+        },
+      },
+    },
+  };
 
   fastify.get("/healthz", opts, async () => {
     const service_discovery = await polarisHealthcheck();
