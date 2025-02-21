0118c2a10f448a6d77d8aa5f2544d367a9d83392
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-02-21 12:51:29 -0800

    Only change values file

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,12 @@
 import fastify_postgres from "@fastify/postgres";
 import { FastifyInstance } from "fastify";
-import { console } from "node:inspector";
 
 export const connect = async (app: FastifyInstance) => {
-  console.log("Connecting to the database");
-  console.log(process.env.JAIP_DB_LOCATION);
+  const db_url = `postgres://${process.env.JAIP_DB_USERNAME}:${process.env.JAIP_DB_PASSWORD}@${process.env.JAIP_DB_LOCATION}:${process.env.JAIP_DB_PORT}/${process.env.JAIP_DB_NAME}`;
+  console.log("CONNECTING TO DATABASE", db_url);
+  console.log("DATABASE_URL", process.env.DATABASE_URL);
   app.register(fastify_postgres, {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL || db_url,
     name: "jaip_db",
   });
 };
