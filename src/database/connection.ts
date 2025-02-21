7083ea3fbb2fcea6cd049ddf0638e96863352cd3
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-02-21 11:51:19 -0800

    Try to connect with just db url

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,10 +1,12 @@
 import fastify_postgres from "@fastify/postgres";
 import { FastifyInstance } from "fastify";
+import { console } from "node:inspector";
 
 export const connect = async (app: FastifyInstance) => {
-  const db_url = `postgres://${process.env.JAIP_DB_USERNAME}:${process.env.JAIP_DB_PASSWORD}@${process.env.JAIP_DB_LOCATION}:${process.env.JAIP_DB_PORT}/${process.env.JAIP_DB_NAME}`;
+  console.log("Connecting to the database");
+  console.log(process.env.JAIP_DB_LOCATION);
   app.register(fastify_postgres, {
-    connectionString: db_url,
+    connectionString: process.env.DATABASE_URL,
     name: "jaip_db",
   });
 };
