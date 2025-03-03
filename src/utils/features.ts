ab352d9b941f5ac8145ad9f47cbbf991df67cc6d
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-03-03 06:36:57 -0800

    Add feature detection functions

---
+++ src/utils/features.ts
@@ -0,0 +1,22 @@
+import { User } from "../types/entities";
+
+// NOTE: This function will return true if the user is associated with any
+// group that has the feature enabled.
+export const user_has_feature = (user: User, feature: string): boolean => {
+  for (const group of user.groups) {
+    if (group.features[feature]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+export const user_has_ungrouped_feature = (
+  user: User,
+  feature: string,
+): boolean => {
+  return (
+    !!user.ungrouped_features[feature] &&
+    !!user.ungrouped_features[feature].enabled
+  );
+};
