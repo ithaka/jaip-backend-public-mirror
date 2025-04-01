0455ab36ec43cea8881b531dae01d575dfe98abd
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-04-01 08:15:58 -0700

    Add route tests

--- src/routes/site_administration/features/grouped/route.test.ts
+++ src/routes/site_administration/features/grouped/route.test.ts
@@ -2,10 +2,42 @@ import { build_test_server } from "../../../../test/helpers";
 
 const app = build_test_server();
 
-test('requests the "/subdomains/validate" route', async () => {
+test('requests the "/grouped/get" route', async () => {
   const res = await app.inject({
-    method: "GET",
-    url: "/subdomains/validate",
+    method: "POST",
+    url: "/grouped/get",
+  });
+  expect(res.statusCode).toEqual(500);
+});
+
+test('requests the "/grouped/reactivate" route', async () => {
+  const res = await app.inject({
+    method: "PATCH",
+    url: "/grouped/reactivate",
+  });
+  expect(res.statusCode).toEqual(500);
+});
+
+test('requests the "/grouped" add route', async () => {
+  const res = await app.inject({
+    method: "POST",
+    url: "/grouped",
+  });
+  expect(res.statusCode).toEqual(500);
+});
+
+test('requests the "/grouped" edit route', async () => {
+  const res = await app.inject({
+    method: "PATCH",
+    url: "/grouped",
+  });
+  expect(res.statusCode).toEqual(500);
+});
+
+test('requests the "/grouped" delete route', async () => {
+  const res = await app.inject({
+    method: "DELETE",
+    url: "/grouped",
   });
   expect(res.statusCode).toEqual(500);
 });
