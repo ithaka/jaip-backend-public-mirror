6f7cdbb4258bfd9a624f8527288cff75919b2b77
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-04-01 11:54:37 -0700

    Update subdomain routes

--- src/routes/site_administration/subdomains/route.test.ts
+++ src/routes/site_administration/subdomains/route.test.ts
@@ -2,10 +2,42 @@ import { build_test_server } from "../../../test/helpers";
 
 const app = build_test_server();
 
-test('requests the "/subdomains/validate" route', async () => {
+test('requests the "/subdomains/get" route', async () => {
   const res = await app.inject({
-    method: "GET",
-    url: "/subdomains/validate",
+    method: "POST",
+    url: "/subdomains/get",
+  });
+  expect(res.statusCode).toEqual(500);
+});
+
+test('requests the "/subdomains/reactivate" route', async () => {
+  const res = await app.inject({
+    method: "PATCH",
+    url: "/subdomains/reactivate",
+  });
+  expect(res.statusCode).toEqual(500);
+});
+
+test('requests the "/subdomains" add route', async () => {
+  const res = await app.inject({
+    method: "POST",
+    url: "/subdomains",
+  });
+  expect(res.statusCode).toEqual(500);
+});
+
+test('requests the "/subdomains" edit route', async () => {
+  const res = await app.inject({
+    method: "PATCH",
+    url: "/subdomains",
+  });
+  expect(res.statusCode).toEqual(500);
+});
+
+test('requests the "/subdomains" delete route', async () => {
+  const res = await app.inject({
+    method: "DELETE",
+    url: "/subdomains",
   });
   expect(res.statusCode).toEqual(500);
 });
