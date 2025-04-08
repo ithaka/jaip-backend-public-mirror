5251e99dd86b781ead3b87e12eae0b0f7a806b43
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-04-08 14:25:35 -0700

    Add new hosted zone for test

--- aws/route53.tf
+++ aws/route53.tf
@@ -1,19 +1,20 @@
 locals {
   app_name = "jaip-backend"
   domain = "pep.jstor.org"
+  temporary_admin_domain = "pep-admin.jstor.org"
+
+  test_domain = "test-pep.jstor.org"
   zone_id = "Z2FDTNDATAQYW2" # This is hardcoded because the hosted zone id for CloudFront is a constant
   cloudfront_dns = "d6u2bqjxuux94.cloudfront.net."
   separator = "."
-  test_prefix = "test"
   admin_prefix = "admin"
   test_environment = "test"
   prod_environment = "prod" 
 }
 
 locals {
-  test_domain = join(local.separator, [local.test_prefix, local.domain])
   admin_domain = join(local.separator, [local.admin_prefix, local.domain])
-  test_admin_domain = join(local.separator, [local.admin_prefix, local.test_prefix, local.domain])
+  test_admin_domain = join(local.separator, [local.admin_prefix, local.test_domain])
   test_provider_prefixes = [
     "subdomain-example",
   ]
@@ -26,11 +27,6 @@ locals {
 }
 
 locals {
-  basic_subdomains = [
-    local.admin_domain,
-    local.test_domain,
-    local.test_admin_domain,
-  ]
   test_provider_subdomains = [
     for provider_prefix in local.test_provider_prefixes : join(local.separator, [
       provider_prefix, local.test_domain
@@ -43,6 +39,16 @@ locals {
   ]
 }
 
+locals {
+  test_subdomains = concat(
+    local.test_provider_subdomains,
+    [local.test_admin_domain],
+  )
+  prod_subdomains = concat(
+    local.prod_provider_subdomains,
+    [local.admin_domain],
+  )
+}
 terraform {
   required_providers {
     aws = {
@@ -59,11 +65,13 @@ terraform {
   }
 }
 
+
+# PRODUCTION
 resource "aws_route53_zone" "zone" {
   name = local.domain
 
   tags = {
-    "ithaka/environment" = "test"
+    "ithaka/environment" = "prod"
     "ithaka/owner"       = "jaip"
     "ithaka/business.unit" = "labs"
     "ithaka/app" = local.app_name
@@ -90,30 +98,15 @@ resource "aws_route53_record" "record" {
   }
 }
 
-# These are all the other basic subdomains (e.g., admin, test, etc.)
-resource "aws_route53_record" "basic_subdomains" {
-  for_each = toset(local.basic_subdomains)
+# This is the main record for the old admin subdomain. This should be
+# set to redirect to the new admin subdomain.
+resource "aws_route53_record" "temporary_admin_record" {
   zone_id = aws_route53_zone.zone.zone_id
-  name    = each.key
+  name    = "${local.temporary_admin_domain}"
   type    = "A"
   alias {
-    name                   = local.cloudfront_dns
-    zone_id                = local.zone_id 
-    evaluate_target_health = true
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-# These are the test provider subdomains. They include the provider subdomain 
-# prefixed to the test subdomain, e.g., subdomain-example.test.jstor.org.
-resource "aws_route53_record" "test_provider_subdomains" {
-  for_each = toset(local.test_provider_subdomains)
-  zone_id = aws_route53_zone.zone.zone_id
-  name    = each.key
-  type    = "A"
-  alias {
+    # NOTE: Do NOT update this until the migration is ready for the final
+    # stage and participants have been notified.
     name                   = local.cloudfront_dns
     zone_id                = local.zone_id 
     evaluate_target_health = true
@@ -124,10 +117,65 @@ resource "aws_route53_record" "test_provider_subdomains" {
 }
 
 # These are the production subdomains. They include the provider subdomain
-# prefixed to the production subdomain, e.g., prod-subdomain-example.jstor.org.
-resource "aws_route53_record" "prod_provider_subdomains" {
-  for_each = toset(local.prod_provider_subdomains)
+# prefixed to the production subdomain, e.g., prod-subdomain-example.pep.jstor.org.
+# The also include the admin subdomain.
+resource "aws_route53_record" "prod_subdomains" {
+  for_each = toset(local.prod_subdomains)
   zone_id = aws_route53_zone.zone.zone_id
+  name    = each.key
+  type    = "A"
+  alias {
+    name                   = local.cloudfront_dns
+    zone_id                = local.zone_id 
+    evaluate_target_health = true
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+
+# TEST
+resource "aws_route53_zone" "test_zone" {
+  name = local.test_domain
+
+  tags = {
+    "ithaka/environment" = "test"
+    "ithaka/owner"       = "jaip"
+    "ithaka/business.unit" = "labs"
+    "ithaka/app" = local.app_name
+    app = local.app_name
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+
+# This is the main record for the test domain.
+resource "aws_route53_record" "test_record" {
+  zone_id = aws_route53_zone.test_zone.zone_id
+  name    = "${local.test_domain}"
+  type    = "A"
+  alias {
+    name                   = local.cloudfront_dns
+    zone_id                = local.zone_id 
+    evaluate_target_health = true
+  }
+  # In general, we don't need to worry about destroying this record, but to 
+  # keep things consistent with prod and to avoid accidents, we'll prevent
+  # destroying it.
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# These are the test provider subdomains. They include the provider subdomain 
+# prefixed to the test subdomain, e.g., subdomain-example.test-pep.jstor.org.
+resource "aws_route53_record" "test_subdomains" {
+  for_each = toset(local.test_subdomains)
+  zone_id = aws_route53_zone.test_zone.zone_id
   name    = each.key
   type    = "A"
   alias {
