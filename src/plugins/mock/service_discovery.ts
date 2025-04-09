9a3521422d7079e3149a0ce99b8bbf3d46534c66
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-04-09 09:31:04 -0700

    Return array in mock service discovery

--- src/plugins/mock/service_discovery.ts
+++ src/plugins/mock/service_discovery.ts
@@ -3,7 +3,7 @@ import service_discovery from "../real/service_discovery";
 const options = {
   discover: jest.fn(() => {
     console.log("Mock discover function called");
-    return "service_discovery";
+    return ["service_discovery", null];
   }),
 };
 
