d072c1aeadc2d0caf425eedbb745ef939cc10f4c
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-04-10 08:46:23 -0700

    Add log

--- src/tests/helpers.ts
+++ src/tests/helpers.ts
@@ -2,6 +2,13 @@ import build from "../build";
 import { RouteSettings } from "../types/routes";
 import { JAIPDatabase } from "../database";
 
+export const dbMock = {
+  get_ip_bypass: jest.fn(),
+  get_first_facility: jest.fn(),
+  get_first_user: jest.fn(),
+  get_sitecode_by_subdomain: jest.fn(),
+} as jest.Mocked<JAIPDatabase>;
+
 export function build_test_server(route_settings: RouteSettings[]) {
   const app = build(
     {
@@ -13,6 +20,7 @@ export function build_test_server(route_settings: RouteSettings[]) {
 
   beforeAll(async () => {
     await app.ready();
+    console.log("Replacing db with mock");
     app.db = dbMock;
   });
 
@@ -20,10 +28,3 @@ export function build_test_server(route_settings: RouteSettings[]) {
 
   return app;
 }
-
-export const dbMock = {
-  get_ip_bypass: jest.fn(),
-  get_first_facility: jest.fn(),
-  get_first_user: jest.fn(),
-  get_sitecode_by_subdomain: jest.fn(),
-} as jest.Mocked<JAIPDatabase>;
