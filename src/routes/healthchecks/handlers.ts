88d917d1944ad7c787278adc60ef65bffad0bb6d
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-04-14 09:43:11 -0700

    Revert "We can ignore the polaris healthcheck because capstan will handle that"
    This reverts commit 946361af526e0d99c9f868cd51dee4a2fef1221b.

--- src/routes/healthchecks/handlers.ts
+++ src/routes/healthchecks/handlers.ts
@@ -1,12 +1,20 @@
 import { FastifyInstance } from "fastify";
-import { db_healthcheck } from "./helpers";
+import { polaris_healthcheck, db_healthcheck } from "./helpers";
 import { ensure_error } from "../../utils";
 
 export const healthchecks_handler = (fastify: FastifyInstance) => async () => {
   try {
+    const service_discovery = await polaris_healthcheck(fastify);
     const db = await db_healthcheck(fastify);
+    fastify.log.info(`Service Discovery Status: ${service_discovery}`);
     fastify.log.info(`Database Status: ${db}`);
 
+    if (!service_discovery) {
+      fastify.event_logger.pep_healthcheck_error(
+        "service_discovery",
+        new Error("Service discovery failed without error"),
+      );
+    }
     if (!db) {
       fastify.event_logger.pep_healthcheck_error(
         "database",
@@ -15,6 +23,7 @@ export const healthchecks_handler = (fastify: FastifyInstance) => async () => {
     }
     return {
       server: true,
+      service_discovery,
       db,
     };
   } catch (err) {
