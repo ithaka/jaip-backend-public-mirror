f004e97748787be90cd08c5d048410ff9719290d
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-04-14 09:14:01 -0700

    Update more  tests

--- src/routes/alerts/route.test.ts
+++ src/routes/alerts/route.test.ts
@@ -1,12 +1,15 @@
 import { build_test_server } from "../../tests/helpers";
 import route_settings from "./routes";
+import { route_schemas } from "./schemas";
+import { get_route } from "../../utils";
 
 const app = build_test_server([route_settings]);
+const prefix = route_settings.options.prefix;
 
 test('requests the "/alerts" route', async () => {
   const res = await app.inject({
     method: "GET",
-    url: "/alerts",
+    url: `${prefix}${get_route(route_schemas.alerts)}`,
   });
   expect(res.statusCode).toEqual(204);
 });
