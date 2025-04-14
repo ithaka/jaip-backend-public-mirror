946361af526e0d99c9f868cd51dee4a2fef1221b
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-04-14 09:37:30 -0700

    We can ignore the polaris healthcheck because capstan will handle that

--- src/routes/healthchecks/helpers.ts
+++ src/routes/healthchecks/helpers.ts
@@ -1,18 +1,5 @@
 import { FastifyInstance } from "fastify";
-import axios from "axios";
 import { ensure_error } from "../../utils";
-
-export const polaris_healthcheck = async (fastify: FastifyInstance) => {
-  const url = "http://localhost:8888/healthcheck";
-  try {
-    const { data, status } = await axios.get(url);
-    return data === "ok" && status === 200;
-  } catch (err) {
-    const error = ensure_error(err);
-    fastify.event_logger.pep_healthcheck_error("polaris", error);
-    return false;
-  }
-};
 
 export const db_healthcheck = async (fastify: FastifyInstance) => {
   try {
