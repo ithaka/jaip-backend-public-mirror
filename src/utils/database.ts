277059e951dbcdaa67d75c47e525278ee6c7552f
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-05-07 06:13:25 -0700

    Use faster method for paginating status search results

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -2,3 +2,16 @@ export const get_db_pagination = (page: number, limit: number) => ({
   skip: (page - 1) * limit,
   take: limit,
 });
+
+export const paginated_array = <T>(arr: T[], limit: number, offset: number): T[] => {
+  if (!arr) return [];
+
+  if (!arr.length) {
+    return [];
+  }
+  if (offset > arr.length - 1) {
+    return [];
+  }
+
+  return arr.slice(Math.min(arr.length - 1, offset), Math.min(arr.length, offset + limit));
+}
