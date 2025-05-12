72b1ae36c10880700cd060aa658b5a22d5b8c1f4
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-05-12 12:23:43 -0700

    Add redirect dns

--- aws/route53.tf
+++ aws/route53.tf
@@ -5,6 +5,12 @@ locals {
   domain = "pep.jstor.org"
   test_domain = "test-pep.jstor.org"
   temporary_admin_domain = "pep-admin.jstor.org"
+  temporary_admin_domain_validation_target = "_570520dd2cc3e84cb25b94e561e45dbd.rdnyqppgxp.acm-validations.aws"
+  admin_validation_target = "_8c77e22a5f96d0fa911ff78b713c3bde.xlfgrmvvlj.acm-validations.aws"
+
+  # Cloudfront
+  cloudfront_redirect = "dphlh8l2kaqiy.cloudfront.net."
+  cloudfront_zone_id = "Z2FDTNDATAQYW2"
 
   # Admin Redirect
   s3_website = "s3-website-us-east-1.amazonaws.com"
@@ -26,6 +32,10 @@ locals {
   # Environments
   test_environment = "test"
   prod_environment = "prod" 
+
+  # Validation Prefixes
+  temporary_admin_domain_prefix = "_eebb83d3fb7e7e1e3fb7d11b926b11c7"
+  new_admin_prefix = "_4959b22ce93d05b6947258fb93acc65e"
 }
 
 locals {  
@@ -52,6 +62,14 @@ locals {
 }
 
 locals {
+  # Validation
+  temporary_admin_domain_validation = join(local.separator, [
+    local.temporary_admin_domain_prefix, local.temporary_admin_domain
+  ]) 
+  admin_validation = join(local.separator, [
+    local.new_admin_prefix, local.admin_domain
+  ]) 
+
   # Built subdomains
   test_provider_subdomains = [
     for provider_prefix in local.test_provider_prefixes : join(local.separator, [
@@ -116,16 +134,28 @@ resource "aws_route53_record" "temporary_admin_record" {
   name    = "${local.temporary_admin_domain}"
   type    = "A"
   alias {
-    # NOTE: Do NOT update this until the migration is ready for the final
-    # stage and participants have been notified.
-    name                   = local.s3_website
-    zone_id                = local.s3_zone_id
-    evaluate_target_health = true
+    name                   = local.cloudfront_redirect
+    zone_id                = local.cloudfront_zone_id 
+    evaluate_target_health = false
   }
   lifecycle {
     # prevent_destroy = true
   }
 }
+
+resource "aws_route53_record" "temporary_admin_record_validation" {
+  zone_id = aws_route53_zone.temporary_admin_zone.zone_id
+  name    = "${local.temporary_admin_domain_validation}"
+  type    = "CNAME"
+  ttl   = 60
+  records = [
+    local.temporary_admin_domain_validation_target
+  ]
+  lifecycle {
+    # prevent_destroy = true
+  }
+}
+
 
 # PRODUCTION
 resource "aws_route53_zone" "zone" {
@@ -174,7 +204,18 @@ resource "aws_route53_record" "prod_subdomains" {
   }
 }
 
-
+resource "aws_route53_record" "admin_record_validation" {
+  zone_id = aws_route53_zone.zone.zone_id
+  name    = "${local.admin_validation}"
+  type    = "CNAME"
+  ttl   = 60
+  records = [
+    local.admin_validation_target
+  ]
+  lifecycle {
+    # prevent_destroy = true
+  }
+}
 # TEST
 resource "aws_route53_zone" "test_zone" {
   name = local.test_domain
