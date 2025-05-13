2eb445648abdd80615e7528e604c4254464cc202
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-05-13 08:21:59 -0700

    Move terraform files and separate locals

---
+++ aws/dns/subdomains.tf
@@ -0,0 +1,18 @@
+locals {  
+  # Subdomain Building
+  test_provider_prefixes = [
+    "subdomain-example",
+  ]
+  # This is where new subdomains can be added. Usually, they should take the form
+  # provider-state, e.g., atlo-la (for ATLO devices in Louisiana) or orijin-ma (for 
+  # Orijin devices in Massachusetts).
+  prod_provider_prefixes = [
+    "prod-subdomain-example", 
+    "orijin-ma",
+    "orijin-ut",
+    "atlo-az",
+    "atlo-fl",
+    "atlo-ks",
+    "atlo-la",
+  ]
+}
